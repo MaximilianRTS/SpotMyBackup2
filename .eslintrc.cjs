a1fc1c1aa17d8a87def62d09eb8ed48e6fd17da2
MaximilianRTS <MaximilianReitsberger@outlook.de>
2026-03-23 12:12:04 +0100

    fix: add missing ESLint config to fix CI lint errors

---
+++ .eslintrc.cjs
@@ -0,0 +1,11 @@
+/* eslint-env node */
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:vue/vue3-essential',
+    '@vue/eslint-config-typescript',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+}
